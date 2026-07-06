f36c129f74fb9b24e804308da67d1a25492eaae9
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:15:30 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "storage_mover_projects" {
   description = "All storage_mover_project resources"
   value       = azurerm_storage_mover_project.storage_mover_projects
 }
+output "storage_mover_projects_description" {
+  description = "List of description values across all storage_mover_projects"
+  value       = [for k, v in azurerm_storage_mover_project.storage_mover_projects : v.description]
+}
+output "storage_mover_projects_name" {
+  description = "List of name values across all storage_mover_projects"
+  value       = [for k, v in azurerm_storage_mover_project.storage_mover_projects : v.name]
+}
+output "storage_mover_projects_storage_mover_id" {
+  description = "List of storage_mover_id values across all storage_mover_projects"
+  value       = [for k, v in azurerm_storage_mover_project.storage_mover_projects : v.storage_mover_id]
+}
 
