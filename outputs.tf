0ed7d19233bd21b65bfc6ee11713384791538c9f
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:21 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "storage_mover_projects_id" {
   description = "Map of id values across all storage_mover_projects, keyed the same as var.storage_mover_projects"
-  value       = { for k, v in azurerm_storage_mover_project.storage_mover_projects : k => v.id }
+  value       = { for k, v in azurerm_storage_mover_project.storage_mover_projects : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "storage_mover_projects_description" {
   description = "Map of description values across all storage_mover_projects, keyed the same as var.storage_mover_projects"
-  value       = { for k, v in azurerm_storage_mover_project.storage_mover_projects : k => v.description }
+  value       = { for k, v in azurerm_storage_mover_project.storage_mover_projects : k => v.description if v.description != null && length(v.description) > 0 }
 }
 output "storage_mover_projects_name" {
   description = "Map of name values across all storage_mover_projects, keyed the same as var.storage_mover_projects"
-  value       = { for k, v in azurerm_storage_mover_project.storage_mover_projects : k => v.name }
+  value       = { for k, v in azurerm_storage_mover_project.storage_mover_projects : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "storage_mover_projects_storage_mover_id" {
   description = "Map of storage_mover_id values across all storage_mover_projects, keyed the same as var.storage_mover_projects"
-  value       = { for k, v in azurerm_storage_mover_project.storage_mover_projects : k => v.storage_mover_id }
+  value       = { for k, v in azurerm_storage_mover_project.storage_mover_projects : k => v.storage_mover_id if v.storage_mover_id != null && length(v.storage_mover_id) > 0 }
 }
 
